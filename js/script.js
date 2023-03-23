715f4c0789a247bb39d340f29706f9be13c44358
Langga Aldika <dikaaldika799@gmail.com>
2023-03-23 18:09:06 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle Class Active Navbar
+const navbarNav = document.querySelector(".navbar-nav");
+
+// When menu is clicked
+document.querySelector("#menu-icon").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Click area to hide sidebar
+const menuIcon = document.querySelector("#menu-icon");
+document.addEventListener("click", function (e) {
+  if (!menuIcon.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
